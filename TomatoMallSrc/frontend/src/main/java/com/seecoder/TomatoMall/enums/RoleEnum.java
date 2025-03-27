5d2650d60c8d5983c7695692ab10a3b98f4dd54d
happytech <happytech.web@gmail.com>
2025-03-27 14:56:57 +0800

    fix: rm main

--- TomatoMallSrc/frontend/src/main/java/com/seecoder/TomatoMall/enums/RoleEnum.java
+++
@@ -1,5 +0,0 @@
-package com.seecoder.TomatoMall.enums;
-
-public enum RoleEnum {
-    CUSTOMER,MANAGER;
-}
--- TomatoMallSrc/frontend/src/main/java/com/seecoder/TomatoMall/service/UtilService.java
+++
@@ -1,8 +0,0 @@
-package com.seecoder.TomatoMall.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UtilService
-{
-    String upload(MultipartFile rawFileContent);
-}
