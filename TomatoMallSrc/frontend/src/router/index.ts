80dee8cb03ce3d5ceb7f18e11696f8ef14993baf
231250096 <231250096@smail.nju.edu.cn>
2025-03-27 11:40:53 +0800

    feature: change router

--- TomatoMallSrc/frontend/src/router/index.ts
+++ TomatoMallSrc/frontend/src/router/index.ts
@@ -7,7 +7,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: '/dashboard'
+            redirect: '/login'
         },
         {
             path: '/login',
